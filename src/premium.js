8fd74142b1dceea991232cb970b74798e2043b66
Sanjusheoran375 <sanjusheoran375@gmail.com>
2024-10-15 16:27:54 +0530

    Update premium.js

--- src/premium.js
+++ src/premium.js
@@ -16,18 +16,18 @@
 //                                                                                                      //
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 //*
-//  * @project_name : XLICON-V4-MD
-//  * @author : salmanytofficial
+//  * @project_name : Sanju-V4-MD
+//  * @author : sanjuofficial
 //  * @youtube : https://www.youtube.com/@s4salmanyt
-//  * @description : XLICON-V4 ,A Multi-functional whatsapp user bot.
+//  * @description : Sanju-V4 ,A Multi-functional whatsapp user bot.
 //*
 //*
-//base by DGXeon
+//base by SNju
 //re-upload? recode? copy code? give credit ya :)
-//Instagram: ahmmikun
-//Telegram: t.me/ahmmitech
-//GitHub: @salmanytofficial
-//WhatsApp: +923184070915
+//Instagram: sanjubhai
+//Telegram: https://t.me/u_r_mental_but_I_like_u
+//GitHub: @Sanjusheoran375
+//WhatsApp: +919728714060
 //want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@DGXeon
 //   * Created By Github: DGXeon.
 //   * Credit To Xeon
